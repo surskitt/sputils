ab424acf33966fc89a39960aec071f40fc60e6f7
sharktamer <donohoe.shane@gmail.com>
2019-06-01 18:08:18 +0100

    add test for album search dict function

--- tests/test_search.py
+++ tests/test_search.py
@@ -11,6 +11,12 @@ from sputils import sputils
 import helpers
 
 
+def test_album_to_dict_searched(api_album_searched, album_dict_searched):
+    album = sputils.album_to_dict_searched(api_album_searched)
+
+    assert deepdiff.DeepDiff(album, album_dict_searched) == {}
+
+
 def test_search_album(sp_mock, api_album_searched, album_dict_searched):
     sp = sp_mock.Spotify()
 
